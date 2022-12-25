6f12a1781ad87354a1e2fab9508ed9022288ae83
nejadipour <nejadipour@gmail.com>
2022-12-25 09:49:14 +0330

    Add base url of the back-end

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<App />, document.getElementById("root"));
