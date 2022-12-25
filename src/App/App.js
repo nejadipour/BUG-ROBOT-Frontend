e5bbcf4f0354bdfb067774fbd25ac824a75cca45
nejadipour <nejadipour@gmail.com>
2022-12-25 19:03:42 +0330

    Add font family and router to the Apps.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,15 @@
-import logo from '../logo.svg'
-import './App.css';
+import '../assets/fonts/css/fontiran.css';
+import { ThemeProvider } from "@material-ui/core";
+import Theme from "../theme/Theme";
+import { BrowserRouter } from "react-router-dom";
+import Router from "../routes/Router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <ThemeProvider theme={Theme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
-
-export default App;
