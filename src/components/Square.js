69545ba96155ed28b9d9975b2aab001b390b5964
nejadipour <nejadipour@gmail.com>
2022-12-26 17:49:05 +0330

    Refactor implementation of Square component for handling clicks

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,23 +1,27 @@
-import { Button, makeStyles } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-    button: {
-        backgroundColor: theme.palette.primary.main,
-        margin: "15px",
-        border: "2px solid " + theme.palette.secondary.main,
-        borderRadius: "15px",
-        boxShadow: "0 12px " + theme.palette.secondary.main + ", 0 18px rgba(0,0,0,0.4)",
-        width: "90px",
-        height: "80px",
-        "&:hover": {
-          backgroundColor: theme.palette.primary.main,
-      },
-    },
-}));
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ChildElement = React.createRef();
+    }
 
-export default function Square(props) {
-    const classes = useStyles();
+    handleClick = () => {
+        console.log(this);
+    };
 
-    return <Button className={classes.button}></Button>;
+    render() {
+        return (
+            <Button
+                className={this.props.className}
+                onClick={() => {
+                    this.handleClick();
+                }}
+            >
+            </Button>
+        );
+    }
 }
+
+export default Square;
