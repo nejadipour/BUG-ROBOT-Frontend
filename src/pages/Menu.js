12166d61c8e094b33e84d66ba33c69dd052d45c7
nejadipour <nejadipour@gmail.com>
2022-12-27 10:59:40 +0330

    Add menu page to the project

---
+++ src/pages/Menu.js
@@ -0,0 +1,93 @@
+import {
+    Box,
+    Button,
+    Grid,
+    List,
+    Typography,
+    makeStyles,
+} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import MenuItem from "../components/items/MenuItem";
+import { Stack } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+    error: {
+        color: theme.palette.error.main,
+        fontWeight: "bold",
+    },
+    createButton: {
+        color: theme.palette.secondary.main,
+        borderColor: theme.palette.secondary.main,
+        borderRadius: "15px",
+        boxShadow: "none",
+    },
+}));
+
+export default function Menu() {
+    const classes = useStyles();
+    const [boards, setBoards] = useState([]);
+    const [error, setError] = useState("");
+    const [render, setRender] = useState(false);
+
+    async function fetchUsers() {
+        try {
+            // const response = await axios.get('')
+            // setBoards(response.data);
+
+            setBoards([
+                {
+                    shape: [4, 4],
+                    name: "test board",
+                },
+                {
+                    shape: [4, 4],
+                    name: "test board2",
+                },
+            ]);
+
+            setRender(true);
+        } catch (error) {
+            setError("مشکلی در دریافت اطلاعات کاربران به وجود آمد");
+            setRender(true);
+        }
+    }
+
+    useEffect(() => {
+        if (!render) fetchUsers();
+    }, [render]);
+
+    return (
+        <Grid container justifyContent="center">
+            {render && (
+                <Stack alignItems="center" direction="column">
+                    <List
+                        sx={{
+                            minWidth: "25vw",
+                            position: "relative",
+                            overflow: "auto",
+                            maxHeight: "60vh",
+                            "& ul": { padding: 0 },
+                        }}
+                        subheader={<li />}
+                    >
+                        {boards.map((board) => (
+                            <MenuItem board={board} />
+                        ))}
+                    </List>
+
+                    <Box padding={2}>
+                        {error !== "" && (
+                            <Typography className={classes.error}>
+                                {error}
+                            </Typography>
+                        )}
+                    </Box>
+
+                    <Button variant="outlined" className={classes.createButton}>
+                        +Add Board
+                    </Button>
+                </Stack>
+            )}
+        </Grid>
+    );
+}
