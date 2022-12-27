500f36743503a73aa4b823ebe7083be96e456a7e
nejadipour <nejadipour@gmail.com>
2022-12-27 09:03:21 +0330

    Add basic implementation of handling clicks on the squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,27 +1,74 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import PestControlIcon from "@mui/icons-material/PestControl";
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.ChildElement = React.createRef();
-    }
+export default function Square(props) {
+    const {
+        position,
+        type,
+        is_occupied,
+        selectedCard,
+        changeCard,
+        setChangeCard,
+        selectedSquare,
+        setSelectedSquare,
+    } = props;
+    const [squareType, setSquareType] = useState(type);
 
-    handleClick = () => {
-        console.log(this);
+    const handleClick = () => {
+        // testing deleting a position
+        if (position === "[0,0]") {
+            setChangeCard("[1,1]");
+        }
+        if (selectedCard) {
+            // add card
+            if (selectedCard === "robot_card") {
+                setSquareType("BOT");
+            } else {
+                setSquareType("BUG");
+            }
+        } else {
+            if (selectedSquare) {
+                // move card
+                setSelectedSquare(null);
+            } else {
+                // select a square
+                if (squareType === "BOT") {
+                    setSelectedSquare(position);
+                }
+            }
+        }
     };
 
-    render() {
-        return (
-            <Button
-                className={this.props.className}
-                onClick={() => {
-                    this.handleClick();
-                }}
-            >
-            </Button>
-        );
-    }
-}
+    useEffect(() => {
+        if (changeCard && changeCard === position) {
+            setSquareType("");
+            setChangeCard(null);
+        }
 
-export default Square;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [changeCard]);
+
+    return (
+        <Button
+            id={position}
+            className={props.className}
+            style={
+                selectedSquare === position
+                    ? { backgroundColor: "#ffa42e" }
+                    : null
+            }
+            onClick={() => {
+                handleClick();
+            }}
+        >
+            {is_occupied && (
+                <>
+                    {squareType === "BOT" && <SmartToyIcon />}
+                    {squareType === "BUG" && <PestControlIcon />}
+                </>
+            )}
+        </Button>
+    );
+}
