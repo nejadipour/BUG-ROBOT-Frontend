2b86b6e6ee5d15d2317c5b909a031055ccdc1e0a
nejadipour <nejadipour@gmail.com>
2022-12-27 17:59:34 +0330

    Update popup style

--- src/components/controls/Popup.js
+++ src/components/controls/Popup.js
@@ -1,27 +1,41 @@
-import { Dialog, DialogTitle, DialogContent, makeStyles, Typography } from '@material-ui/core';
-import Controls from "./controls/Controls";
-import CloseIcon from '@material-ui/icons/Close';
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    makeStyles,
+    Typography,
+    Button,
+} from "@material-ui/core";
+import CloseIcon from "@mui/icons-material/Close";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
     dialogWrapper: {
-        maxHeight: '80vh',
+        backgroundColor: theme.palette.background.default,
+        maxHeight: "80vh",
         padding: theme.spacing(1),
-        position: 'absolute',
+        position: "absolute",
         top: theme.spacing(5),
-        maxWidth: '100vw'
+        maxWidth: "100vw",
     },
     dialogTitle: {
-        paddingLeft: '0px'
-    }
-}))
+        color: theme.palette.primary.main,
+        paddingLeft: theme.spacing(3),
+    },
+}));
 
 export default function Popup(props) {
-
-    const { title, children, openPopup, setOpenPopup, disableClickOutClose, disableClose } = props;
+    const {
+        title,
+        children,
+        openPopup,
+        setOpenPopup,
+        disableClickOutClose,
+        disableClose,
+    } = props;
     const classes = useStyles();
 
     const handleClose = () => {
-        setOpenPopup(false)
+        setOpenPopup(false);
     };
 
     return (
@@ -29,28 +43,30 @@ export default function Popup(props) {
             open={openPopup}
             maxWidth="md"
             classes={{ paper: classes.dialogWrapper }}
-            onClose={(disableClickOutClose || disableClose) ? null : handleClose}
+            onClose={disableClickOutClose || disableClose ? null : handleClose}
         >
             <DialogTitle className={classes.dialogTitle}>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    
-                    {!disableClose ?
-
-                        <Controls.ActionButton
-                            color="secondary"
-                            onClick={handleClose}>
-                            <CloseIcon />
-                        </Controls.ActionButton>
-                        : <div/>
-                    }
-                    <Typography variant="h6" component="div" style={{ marginRight: '16px' }}>
+                <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        style={{ marginRight: "16px" }}
+                    >
                         {title}
                     </Typography>
+
+                    {!disableClose ? (
+                        <Button color="secondary" onClick={handleClose}>
+                            <CloseIcon />
+                        </Button>
+                    ) : (
+                        <div />
+                    )}
                 </div>
             </DialogTitle>
-            <DialogContent dividers>
-                {children}
-            </DialogContent>
+            <DialogContent dividers>{children}</DialogContent>
         </Dialog>
-    )
+    );
 }
