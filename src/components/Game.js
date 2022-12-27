24263e76d33c2d05f5328f2c2030d58414d21649
nejadipour <nejadipour@gmail.com>
2022-12-27 12:32:08 +0330

    Add ability to read board from the state in the game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import { Button, Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import Board from "./Board";
 import Stack from "@mui/material/Stack";
+import { useLocation } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
     deleteButton: {
@@ -17,11 +18,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Game(props) {
     const classes = useStyles();
+    const location = useLocation();
 
     return (
         <Grid container justifyContent="center" md={12} sm={12}>
             <Grid item md={12} sm={12}>
-                <Board boardId={props.boardId} />
+                <Board board={location.state?.board} />
             </Grid>
             <Stack marginTop={10} marginBottom={15} direction="row" spacing={2}>
                 <Button variant="outlined" className={classes.menuButton}>
